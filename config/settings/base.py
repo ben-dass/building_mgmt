63863a10ccb44727f5b9d2f59449a540ee08719b
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2025-02-14 12:45:26 -0500

    added logging on dev level

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,4 +1,4 @@
-from os import path
+from os import getenv, path
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST"),
+        "PORT": getenv("POSTGRES_PORT"),
     }
 }
 
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -23,3 +23,24 @@ ALLOWED_HOSTS = [
 
 ADMIN_URL = getenv("DJANGO_ADMIN_URL")
 DOMAIN = getenv("DOMAIN")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)s %(asctime)s %(module)s %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+}
